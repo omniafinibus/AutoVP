c0c6ac174d3cfc7ca0c2619634a6a808d070fce3
omniafinibus <arjanmail@tutamail.com>
2024-03-25 18:15:59 +0100

    added bug to directory definitions

--- scripts/_directory.py
+++ scripts/_directory.py
@@ -1,7 +1,7 @@
 import os
 
 PROJECT_NAME = "autovp"
-SOURCE_DIRECTORY = os.path.join("home", "arjan", "Nextcloud", "Elektrotechniek", "MSE-Q3", "ElDA_5SIB0", "AutoVP")
+SOURCE_DIRECTORY = os.path.join(ENTER DIRECTORY HERE)
 MAIN_DIRECTORY = os.path.join(SOURCE_DIRECTORY, PROJECT_NAME)
 VENV_DIRECTORY = os.path.join(SOURCE_DIRECTORY, ".venv")
 PROJECT_DIRECTORY = os.path.join(MAIN_DIRECTORY, PROJECT_NAME)
